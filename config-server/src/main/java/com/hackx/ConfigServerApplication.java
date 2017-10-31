b66272c92d4e0f41f7feae05c2df796c64b88150
qianqian <qianqian20170929@gmail.com>
2017-10-31 15:50:21 +0800

    make config server realiable

--- config-server/src/main/java/com/hackx/ConfigServerApplication.java
+++ config-server/src/main/java/com/hackx/ConfigServerApplication.java
@@ -3,7 +3,10 @@ package com.hackx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
